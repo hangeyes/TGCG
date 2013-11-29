9999fe0bcc09e368122fcd24e5182ce43220ecd4
Xkonti <online.xkonti@gmail.com>
2013-11-29 22:19:08 +0100

    Filesystem Setup
    Dodanie main.lua
    Dodanie bibliotek HUMP

---
+++ main.lua
@@ -0,0 +1,41 @@
+function love.load()
+
+	Class = require("libs.humpClass")
+	
+end
+
+function love.update(dt)
+
+	-- your code
+	
+end
+
+function love.draw()
+
+	-- your code
+	
+end
+ 
+function love.mousepressed(x, y, button)
+
+	-- your code
+	
+end
+ 
+function love.mousereleased(x, y, button)
+
+	-- your code
+	
+end
+ 
+function love.keypressed(key, unicode)
+
+	-- your code
+	
+end
+ 
+function love.keyreleased(key)
+
+	-- your code
+	
+end
