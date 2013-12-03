3041801a5ed8e8301cb53132c014f6404b1b8b6f
Xkonti <online.xkonti@gmail.com>
2013-12-03 11:52:42 +0100

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -18,30 +18,54 @@ Button = Class
 {
 	----- Atrybuty
 	
-	-- text		-- Tekst wyświetany przez przycisk
+	-- text			-- Tekst wyświetany przez przycisk
 	-- x			-- Pozycja X
 	-- y			-- Pozycja Y
-	-- width	-- Szerokość przycisku
-	-- height	-- Wysokość przycisku
+	-- width		-- Szerokość przycisku
+	-- height		-- Wysokość przycisku
 	
-	-- visible	-- Czy przycisk jest widoczny
-	-- enabled	-- Czy przycisk jest aktywny
+	-- visible		-- Czy przycisk jest widoczny
+	-- enabled		-- Czy przycisk jest aktywny
 	
 	----- Funkcje
 	
-	-- Init (text, x, y, width, height)	-- Konstruktor
+	-- Init (text, x, y, width)	-- Konstruktor
 	
 	-- SetPos (x, y)
 	-- SetVisible (bool)
 	-- SetEnabled (bool)
+	-- SetText (text)
+	
+	-- Draw ()
 }
 
-function Button:Init (text, x, y, width, height)
-	self.text		= text
-	self.x			= x
-	self.y			= y
+function Button:Init (text, x, y, width)
+	self.text	= text
+	self.x		= x
+	self.y		= y
 	self.width	= width
-	self.height	= height
+	self.height	= 32
 	self.visible= true
 	self.enabled= true
+end
+
+function Button:SetPos (x, y)
+	self.x = x
+	self.y = y
+end
+
+function Button:SetVisible (bool)
+	self.visible = bool;
+end
+
+function Button:SetEnabled (bool)
+	self.enabled = bool
+end
+
+function Button:SetText (text)
+	self.text = boot
+end
+
+function Button:Draw ()
+	
 end
