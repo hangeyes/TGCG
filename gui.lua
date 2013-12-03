ab5e333cd920759cb679b97cb119acc4db522be6
Xkonti <online.xkonti@gmail.com>
2013-12-03 11:29:59 +0100

    + Klasa Button
    Wstęp

--- gui.lua
+++ gui.lua
@@ -8,3 +8,40 @@
 -- List
 -- OptionCheck
 -----------------------------------------------
+
+-----------------------------------------------
+-- BUTTON -------------------------------------
+-----------------------------------------------
+
+-- Deklaracja klasy
+Button = Class
+{
+	----- Atrybuty
+	
+	-- text		-- Tekst wyświetany przez przycisk
+	-- x			-- Pozycja X
+	-- y			-- Pozycja Y
+	-- width	-- Szerokość przycisku
+	-- height	-- Wysokość przycisku
+	
+	-- visible	-- Czy przycisk jest widoczny
+	-- enabled	-- Czy przycisk jest aktywny
+	
+	----- Funkcje
+	
+	-- Init (text, x, y, width, height)	-- Konstruktor
+	
+	-- SetPos (x, y)
+	-- SetVisible (bool)
+	-- SetEnabled (bool)
+}
+
+function Button:Init (text, x, y, width, height)
+	self.text		= text
+	self.x			= x
+	self.y			= y
+	self.width	= width
+	self.height	= height
+	self.visible= true
+	self.enabled= true
+end
